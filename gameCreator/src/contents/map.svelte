6f4dda66b1195a2eaaee169095906e270eda1121
pl4cydo <placydo.doc@gmail.com>
2022-09-07 14:04:27 -0300

    testando criação do mapa

---
+++ gameCreator/src/contents/map.svelte
@@ -0,0 +1,50 @@
+<script>
+	let name = 'teste';
+	let map = [
+		'##########',
+		'######0###',
+		'######0###',
+		'######0###',
+		'###0######',
+		'###0######',
+		'###0######',
+	];
+    
+	
+</script>
+
+<table>
+	{#each map as row, i}
+	<tr>
+		{#each [...row] as ch, j}
+            {#if ch == '#'}
+                <img src="./images/IndustrialTile_03.png" alt="flor">
+            {:else if ch != '#'}
+                <img src="./images/IndustrialTile_01.png" alt="">
+            {/if}
+		{/each}
+	</tr>
+	{/each}
+</table>
+
+<style>
+	td {
+		width: 50px;
+		height: 50px;
+	}
+    img {
+        width: 50px;
+		height: 50px;
+        background: #333;
+        margin-top: 0;
+        padding: 0;
+    }
+	.wall {
+        background: #333;
+	}
+	table{
+        padding: 0;
+		margin: auto;
+		transform: perspective(400px) rotateX(30deg)
+	}
+ </style>
--- gameCreator/src/main.js
+++ gameCreator/src/main.js
@@ -1,8 +1,7 @@
-import './app.css'
-import App from './App.svelte'
+import App from './App.svelte';
 
-const app = new App({
-  target: document.getElementById('app')
-})
+var app = new App({
+	target: document.body
+});
 
-export default app
+export default app;
