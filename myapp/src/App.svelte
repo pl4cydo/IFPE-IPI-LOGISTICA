27729da6934c382f69f7c766af6ec5869943f2be
Arluzss <arleynunes251@gmail.com>
2022-09-09 20:28:45 -0300

    Movimento Base feito
    antes nao andava agr anda

--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -9,68 +9,92 @@ onMount(()=>{
   canvas.height = 500;
   const c = canvas.getContext('2d');
   c.fillRect(0,0, canvas.width, canvas.height);
+  var valor = {x:0, y:0,s:0,m:0};
+
   class teste {
-  constructor({position, velocity}){
+    constructor({position, velocity}){
     this.position = position;
     this.velocity = velocity;
-  }
-  draw(){
+    }
+
+   draw(){
     c.fillStyle = 'red'
     c.fillRect(this.position.x, this.position.y, 50, 50)
-  }
-  update(){
+   }
+    update(){
     this.draw();
-    this.position.x += this.position.x
-    this.position.y += this.position.y
 
-  }
-}
-
-const Player = new teste({
-    position:{
-      x:0,
-      y:0
-    },
-    velocity:{
-      x:0,
-      y:0
+    this.position.x = this.velocity.x
+    this.position.y = this.velocity.y
+   
     }
-  
-  });//iscopu player
-  document.addEventListener('click', pClick, true);
-  function pClick(e){
-    window.requestAnimationFrame(pClick)
-    click = {x:e.pageX, y:e.pageY};
-    console.log(click)
-    if(Player.velocity.x >= click.x){
-	    bolea.x = false
-	}else{
-	    bolea.x = true
-}
-//teste eixo y de lado
-	if(Player.velocity.y >= click.y){
-	    bolea.y = false
-	}else{
-	    bolea.y = true
-}
-//xxxx
-    if(Player.velocity.x < click.x && bolea.x == true ){
-		  Player.velocity.x = 5;
 
-		 } else if(click.x < Player.velocity.x && bolea.x == false){
-			 Player.velocity.x = -5;
-		 }
-//yyyyy
-  if(Player.velocity.y < click.y && bolea.y == true){
-			 Player.velocity.y = 5;
-					 
-		 }else if(click.y < Player.velocity.y && bolea.y == false){
-			 Player.velocity.y = -5;	 
-		 }
-    Player.update();
-    console.log("xxxx",Player.velocity.x,"YYY", Player.velocity.y)
   }
+  const player = new teste({
+      position:{
+        x:0,
+        y:0
+      },
+      velocity:{
+        x:0,
+        y:0
+      }
+    
+    });//iscopu player
+
+   function animate(){
+      window.requestAnimationFrame(animate)
+      c.fillStyle = 'black'
+      c.fillRect(0, 0, canvas.width, canvas.height)
+
+      if(player.velocity.x < valor.x && bolea.x == true){
+        valor.s += 2
+        player.velocity.x = valor.s
+
+      }else if(valor.x < player.velocity.x && bolea.x == false){
+        valor.s -= 2
+        player.velocity.x = valor.s
+      }
+      
+      if(player.velocity.y < valor.y && bolea.y == true){
+        valor.m += 2
+        player.velocity.y = valor.m
+        
+      }else if(valor.y < player.velocity.y && bolea.y == false){
+        valor.m -= 2
+        player.velocity.y = valor.m
+      }
+      player.update()
+    }
+    
+    animate()
+
+    window.addEventListener('click', (e) =>{
+    click = {x:e.pageX, y:e.pageY}
+    valor.x = click.x
+    valor.y = click.y
+
+    if(player.velocity.x >= click.x){
+	  bolea.x = false
+	  console.log("Xmaior ou igual")
+	  }else{
+	  bolea.x = true
+	  console.log("Xmenor ou igual")
+  }
+//teste eixo y de lado
+	  if(player.velocity.y >= click.y){
+	  bolea.y = false
+	  console.log("Ymaior ou igual")
+	  }else{
+  	bolea.y = true
+	  console.log("Ymenor ou igual")
+  }
+    })
+
+
 })//iscopu onMount
+
+
 
 </script>
 <canvas
