bc1476d6d0eb0c63bc9dce2b663e8e2cb4b71d38
Arluzss <arleynunes251@gmail.com>
2022-09-11 17:27:38 -0300

    adiçao  de mapa teste, barreiras ao mapa e localizaçao das barreiras

---
+++ myapp/Gameassets/barrier.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.1" name="barrier" tilewidth="111" tileheight="128" tilecount="1" columns="1">
+ <image source="barrier.png" width="111" height="128"/>
+</tileset>
--- myapp/src/App.svelte
+++ myapp/src/App.svelte
@@ -1,35 +1,81 @@
 <script>
-import { onMount } from 'svelte';
+  import { onMount } from 'svelte';
+  import collisions from './collisions'
 let canvas;
 
 onMount(()=>{
   var click;
-  var bolea = {x:false,y:false};
-  canvas.width = 500;
-  canvas.height = 500;
+  var bolea = {x:false,y:false, o:false};
+  canvas.width = 1080;
+  canvas.height = 900;
   const context = canvas.getContext('2d');
-  context.fillRect(0,0, canvas.width, canvas.height);
   var valor = {x:0, y:0,s:0,m:0};
+ 
 
   class teste {
+    
     constructor({position, velocity}){
     this.position = position;
     this.velocity = velocity;
+    
     }
 
    draw(){
     context.fillStyle = 'red'
     context.fillRect(this.position.x, this.position.y, 50,50)
+    
    }
     update(){
     this.draw();
-
     this.position.x = this.velocity.x
     this.position.y = this.velocity.y
    
     }
 
   }
+ 
+  class outro{
+    static largura (){
+        return 44
+    } 
+    static altura (){
+      return 44
+    }
+    constructor({position}){
+      this.position = position
+      this.altura = 44
+      this.largura = 44
+
+    }
+    drau(){
+      context.fillStyle = 'green'
+      context.fillRect(this.position.x, this.position.y, this.largura, this.altura)
+    }
+  }
+
+  const colisaoMapa = []
+  for(let i = 0; i < collisions.length; i += 35){
+    colisaoMapa.push(collisions.slice(i, 35 + i))
+  }
+  
+  const guardColis = []
+  colisaoMapa.forEach((linha, i) => {
+    linha.forEach((sim, j) => {
+      if(sim === 3)
+      guardColis.push(
+        new outro ({
+          position:{
+            x:j * outro.largura(),
+            y:i * outro.altura()
+          }
+        })
+      )
+    })
+  })
+
+  const image = new Image()
+  image.src = './Gameassets/map.png'
+
   const player = new teste({
       position:{
         x:0,
@@ -44,13 +90,15 @@ onMount(()=>{
 
    function animate(){
       window.requestAnimationFrame(animate)
-      context.fillStyle = 'black'
-      context.fillRect(0, 0 ,canvas.width, canvas.height)
+      context.drawImage(image,-750,-200)
+      guardColis.forEach(outro => {
+        outro.drau()
+      })
 
       if(player.velocity.x < valor.x && bolea.x == true){
         valor.s += 2
         player.velocity.x = valor.s
-
+        
       }else if(valor.x < player.velocity.x && bolea.x == false){
         valor.s -= 2
         player.velocity.x = valor.s
@@ -64,7 +112,6 @@ onMount(()=>{
         valor.m -= 2
         player.velocity.y = valor.m
       }
-      console.log(player.position)
       player.update()
     }
     
@@ -102,12 +149,14 @@ onMount(()=>{
   	bolea.y = true
 	  console.log("Ymenor ou igual")
   }
-    })
+    })//escopo do click
 
 
 })//escopo do onMount
 
+function barrar(){
 
+}
 
 </script>
 <canvas
