f910f45393241ea89b68d05ee2b98e59fae5f47f
pl4cydo <placydo.doc@gmail.com>
2022-09-14 23:32:02 -0300

    teste de mapa

--- gameCreator/src/contents/map.svelte
+++ gameCreator/src/contents/map.svelte
@@ -1,126 +1,45 @@
 <script>
-	let name = 'teste';
-	let map = [
-		'##########',
-		'######0###',
-		'######0###',
-		'######0###',
-		'###0######',
-		'###0######',
-		'###0######',
-	];
-    
-    // function animation 
+	import { onMount } from 'svelte';
 	
+	let canvas;
+	
+	onMount(() => {
+        canvas.width = 1024;
+        canvas.height = 576;
+		const ctx = canvas.getContext('2d');
+		
+		ctx.fillStyle = 'white';
+		ctx.fillRect(0,0,canvas.width, canvas.height);
+
+        const image = new Image();
+        image.src= './images/MapaAlpha2.png'    
+
+        image.onload = () => {
+            ctx.drawImage(image, canvas.width/8, canvas.height/8)
+        }
+
+	})
+	
+
 </script>
 <main>
-    <img class="background" src="./images/Background.png" alt="">
-    <table>
-        {#each map as row, i}
-        <tr>
-            {#each [...row] as ch, j}
-                {#if ch == '#'}
-                    <img class="images" src="./images/IndustrialTile_03.png" alt="flor">
-                {:else if ch != '#'}
-                    <img class="images" src="./images/IndustrialTile_01.png" alt="">
-                {/if}
-            {/each}
-        </tr>
-        {/each}
-    </table>
-    <div class="sprite">
-        <img class="hammer" src="./images/Hammer.png" alt="">
-    </div>
-    <div class="mario">
-        <img class="mario-img" src="./images/_.png" alt="">
-    </div>
+    <body>
+        <canvas bind:this={canvas} id="desenho"></canvas>
+
+    </body>
 </main>
 
-
 <style>
-    body {
-        padding: 0%;
-        margin: 0%;
-    }
-    main {
-        justify-content: center;
-        align-items: center;
-        display: flex;
-    }
-    table {
-        position: absolute;
-    }
-    .background {
-        width: 100%;
-		height: 100vh;
-    }
-	td {
-		width: 50px;
-		height: 50px;
+	body {
+		/* display: flex;
+        flex-direction: row;
+		justify-content: center;
+		align-items: center; */
+		
 	}
-    tr {
-        padding: 0%;
-        margin: 0%;
-    }
-    .images {
-        width: 70px;
-		height: 70px;
-        background: #333;
-        margin-top: -5px;
-        padding: 0;
-        /* position: absolute; */
-     } 
-	.wall {
-        background: #333;
+	canvas {
+        /* position: absolute;  */
+		border: 1px solid black;
+		/* transform: perspective(400px) rotateX(20deg); */
 	}
-	table{
-        position: absolute;
-		margin: auto;
-		transform: perspective(400px) rotateX(20deg)
-	} 
-
-    .sprite {
-        width: 32px;
-        height: 64px;
-        position: absolute;
-        /* border: 1px solid black; */
-        overflow: hidden;
-        margin-left: -75px;
-        margin-top: -20px;
-        transform: perspective(400px) rotateX(-40deg);
-    }
-    .hammer {
-        animation: moviment 2s steps(8, end) infinite;
-    }
-    .mario {
-        width: 50px;
-        height: 64px;
-        position: absolute;
-        /* border: 1px solid black; */
-        overflow: hidden;
-        margin-left: 200px;
-        margin-top: -20px;
-        /* transform: perspective(400px) rotateX(-40deg); */
-    }
-    .mario-img {
-        width: 700px;
-        height: 700px;
-        animation: mariopassos .4s steps(2, end) infinite;
-    }
-    @keyframes moviment {
-        from {
-            margin-left: 0px;
-        }
-        to {
-            margin-left: -256px;
-        }
-    }
-    @keyframes mariopassos {
-        from {
-            margin-left: 0px;
-        }
-        to {
-            margin-left: -150px;
-        }
-    }
- </style>
+</style>
