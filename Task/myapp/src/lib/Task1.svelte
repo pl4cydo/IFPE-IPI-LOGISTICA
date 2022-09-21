8c629f568112bfeb08e6e48d6a55f66557042d02
Anthony Richard <anthonyricharddasilvamartins2@gmail.com>
2022-09-21 16:14:33 -0300

    Mudanças da missão.
    Atualizações:
    
    -  variáveis para travar click adicionadas.
    -  função de checagem das caixas  criada.
    -  caixas retangulares adicionadas ao armazém para a checagem.
    -  mudança no layout da montagem do pallet.

--- Task/myapp/src/lib/Task1.svelte
+++ Task/myapp/src/lib/Task1.svelte
@@ -2,9 +2,11 @@
     <link rel="stylesheet" href="./styles/Task1.css">
 </svelte:head>
 <script>
+    let areaLock = false
     let clickLock = true
     let selectedBox
-    let palletArea
+    let selectedArea
+
     
 
     function openStorage(){
@@ -20,25 +22,30 @@
     function selectBox(){
         if(clickLock == true){
             selectedBox = this
-            console.log(selectedBox)
             clickLock = false
-
-        }
-        else{
-            console.log('caixa ja selecionada')
-        }
-        
+            areaLock = true
+        }        
     }
-    function checkMatch(){
-        palletArea = this
 
-        if(palletArea.dataset.Type == selectedBox.dataset.Type){
-            palletArea.style.backgroundImage = "url(/images/box2.png)";
-            palletArea = null
-            selectedBox = null
+    function selectArea(){
+        selectedArea = this        
+        checkMatch()
+    }
+
+    function checkMatch(){
+        if(areaLock){
+            var ImG = selectedArea.children[0]
+            if(selectedArea.dataset.Type == selectedBox.dataset.Type){
+                selectedArea.classList.add("clickLock")
+                selectedBox.style.display = "none"
+                ImG.style.visibility= "visible"
+            }
         }
-        console.log(palletArea)
-        console.log(selectedBox)
+        clickLock = true
+        areaLock = false
+        selectedBox = null
+        selectedArea = null
+        ImG = null
     }
 </script>
     
@@ -48,26 +55,21 @@
     <div class="taskScreen">
         <img on:click={openStorage} id="Ground_box" src="/images/boxes2.png" alt="Boxes">
         <div id="taskPallet">
-            
             <div id="palletBoxes">
-                <div id="layer1">
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                </div>
-                <div id="layer2">
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                </div>
-                <div id="layer3">
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
-                    <div on:click={checkMatch} class="bx" data-Type="squareBox"></div>
+                <div class="teste">
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
+                    <div on:click={selectArea} class="bx" data-Type="squareBox"><img class="boxImage" src="/images/box2.png" alt=""></div>
                 </div>
                 <img id="palletTask" src="./images/pallet-250px.png" alt="">
-
             </div>
+
         </div> <!--taskpallet-->>
         
         <div id="storage">
@@ -82,10 +84,16 @@
                 <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/box2.png" alt="box"></div>
                 <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/box2.png" alt="box"></div>
                 <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/box2.png" alt="box"></div>
+                <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/rect80px.png" alt="box"></div>
+                <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/rect80px.png" alt="box"></div>
+                <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/rect80px.png" alt="box"></div>
+                <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/rect80px.png" alt="box"></div>
+                <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/rect80px.png" alt="box"></div>
+                <div on:click={selectBox} data-Type="squareBox" class="boxCont"><img src="/images/rect80px.png" alt="box"></div>
             </div>
         </div> <!--storage-->
     </div>
-    <img id="worker" src="./images/workerr.png" alt="worker">
+    <img id="worker" src="./images/worker.png" alt="worker">
     <div class="dialogueBox">
     
     </div>
