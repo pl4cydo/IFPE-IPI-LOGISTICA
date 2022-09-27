f9eb1b6a0164630398b0f4fa255c8a703432fc54
pl4cydo <placydo.doc@gmail.com>
2022-09-27 00:06:01 -0300

    estudando animações de movimentos

--- gameCreator/src/contents/map.svelte
+++ gameCreator/src/contents/map.svelte
@@ -37,15 +37,29 @@
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
 
+    const arrayPlayerImage = ['./images/redSpriteDOWN.png', './images/redSpriteLEFT.png', './images/redSpriteUP.png', './images/redSpriteRIGTH.png']
+
 	onMount(() => { // chamando a função onde o canvas vai ser posto
         canvas.width = 1024; // tamanho da largura do canvas
         canvas.height = 720; // tamanho da altura do canvas
 		const c = canvas.getContext('2d'); // criando uma variavel e chamando o canvas para declarar o contexto 2D
+        let lastKey = ''
+
 
         const imageMap = new Image();
         imageMap.src= './images/mini-mapa.png' 
-        const imagePlayer = new Image(); 
-        imagePlayer.src = './images/redSpriteDOWN.png'
+
+        const imageDOWNPlayer = new Image(); 
+        imageDOWNPlayer.src = './images/redSpriteDOWN.png'
+        
+        const imageUPPlayer = new Image(); 
+        imageUPPlayer.src = './images/redSpriteUP.png'
+
+        const imageLEFTPlayer = new Image(); 
+        imageLEFTPlayer.src = './images/redSpriteLEFT.png'
+
+        const imageRIGHTPlayer = new Image(); 
+        imageRIGHTPlayer.src = './images/redSpriteRIGHT.png'
 
         const boundariesObjets = [];
 
@@ -54,10 +68,9 @@
             y: -370
         }
 
-        let lastKey = ''
 
         class Sprite { 
-            constructor({position, velocity, image, frames = {max: 1}}){
+            constructor({position, velocity, image, frames = {max: 1}, sprites}){
                 this.position = position;
                 this.image = image;
                 this.frames = { 
@@ -70,6 +83,7 @@
                     this.height = this.image.height
                 }
                 this.moving = false 
+                this.sprites = sprites
             }
 
             draw(){
@@ -85,15 +99,14 @@
                     this.image.height, 
                 )
 
-                if (this.moving) {
-                    if ( this.frames.max > 1) {
-                        this.frames.elapsed++
-                    }
+                if (!this.moving) return
+                if ( this.frames.max > 1) {
+                    this.frames.elapsed++
+                }
 
-                    if  ( this.frames.elapsed % 10 == 0) {
-                        if (this.frames.val < this.frames.max - 1) this.frames.val++
+                if  ( this.frames.elapsed % 10 == 0) {
+                    if (this.frames.val < this.frames.max - 1) this.frames.val++
                         else this.frames.val = 0
-                    }
                 }
             }
         }
@@ -105,7 +118,7 @@
                 this.height = 38.4;
             }
             draw(){
-             c.fillStyle = 'rgba(255, 0, 0, 0.1)'
+             c.fillStyle = 'rgba(255, 0, 0, 0)'
              c.fillRect(this.position.x, this.position.y, this.width, this.height)
             }
         }
@@ -132,11 +145,16 @@
                 x: canvas.width/2 - 256/4,
                 y: canvas.height/2 - 256/4
             },
-            image: imagePlayer,
+            image: imageDOWNPlayer,
             frames: {
                 max: 4
-            }
-
+            },
+            sprites: {
+                    down: imageDOWNPlayer,
+                    up: imageUPPlayer,
+                    left: imageLEFTPlayer,
+                    right: imageRIGHTPlayer
+                }
         })
 
         const background = new Sprite({ 
@@ -160,7 +178,7 @@
             return (
                     rectangle1.position.x + rectangle1.width - 17 >= rectangle2.position.x 
                     && rectangle1.position.x + 17 <= rectangle2.position.x + rectangle2.width 
-                    && rectangle1.position.y + rectangle1.height >= rectangle2.position.y 
+                    && rectangle1.position.y + rectangle1.height - 5 >= rectangle2.position.y 
                     && rectangle1.position.y + 13 <= rectangle2.position.y + rectangle2.height
                     )
         }
@@ -178,7 +196,10 @@
             
             
             let moving = true
+            player.moving = false
             if (keys.w.pressed && lastKey === 'w') {
+                player.moving = true
+                player.image = player.sprites.up
                 for (let i = 0; i < boundariesObjets.length; i++){
                     const limitz = boundariesObjets[i]
                     if (
@@ -199,10 +220,12 @@
                 }
                 if(moving) {
                     movables.forEach(jorge => {
-                        jorge.position.y += 3
+                        jorge.position.y += 1.7
                     })
                 }
             } else if (keys.s.pressed && lastKey === 's') {
+                player.moving = true
+                player.image = player.sprites.down
                 for (let i = 0; i < boundariesObjets.length; i++){
                     const limitz = boundariesObjets[i]
                     if (
@@ -223,10 +246,12 @@
                 }
                 if(moving) {
                     movables.forEach(jorge => {
-                        jorge.position.y -= 3
+                        jorge.position.y -= 1.7
                     })
                 }
             } else if (keys.a.pressed && lastKey === 'a') {
+                player.moving = true
+                player.image = player.sprites.left
                 for (let i = 0; i < boundariesObjets.length; i++){
                     const limitz = boundariesObjets[i]
                     if (
@@ -247,10 +272,12 @@
                 }
                 if(moving) {
                     movables.forEach(jorge => {
-                        jorge.position.x += 3
+                        jorge.position.x += 1.7
                     })
                 }
             } else if (keys.d.pressed && lastKey === 'd') {
+                player.moving = true
+                player.image = player.sprites.right
                 for (let i = 0; i < boundariesObjets.length; i++){
                     const limitz = boundariesObjets[i]
                     if (
@@ -271,7 +298,7 @@
                 }
                 if(moving) {
                     movables.forEach(jorge => {
-                        jorge.position.x -= 3
+                        jorge.position.x -= 1.7
                     })
                 }
             }    
