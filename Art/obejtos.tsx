3698ad118e7031a55ede0073b2c0dfdeb514aecc
pl4cydo <placydo.doc@gmail.com>
2022-11-14 18:41:42 -0300

    é isso ai

--- Art/obejtos.tsx
+++ Art/obejtos.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.1" name="objetos" tilewidth="32" tileheight="32" tilecount="1424" columns="16">
- <image source="../../../../Downloads/Modern tiles_Free/Interiors_free/32x32/Interiors_free_32x32.png" width="512" height="2848"/>
+<tileset version="1.8" tiledversion="1.8.0" name="caixa2" tilewidth="32" tileheight="32" tilecount="576" columns="24">
+ <image source="../../../../Imagens/testandocaixa2.png" width="768" height="768"/>
 </tileset>
--- gameCreator/src/App.svelte
+++ gameCreator/src/App.svelte
@@ -1,10 +1,12 @@
 <script>
-	import MapTest from './backup/mapTest.svelte'
+	import MapTest from './backup/mapTest2.svelte'
   import Map from './contents/map.svelte'
+  import Map3 from './backup/map3.svelte'
 </script>
 
 <main>
   <!-- <Array/> -->
   <!-- <Map/> -->
-  <MapTest/>
+  <!-- <MapTest/> -->
+  <Map3/>
 </main>
