7ac8f460e67710ca3baff3107aaf1692b7e4fa89
pl4cydo <placydo.doc@gmail.com>
2022-12-09 17:19:45 -0300

    Teste de envio

--- Mapa/src/tools/Ranking.svelte
+++ Mapa/src/tools/Ranking.svelte
@@ -2,7 +2,7 @@
     import { onMount } from "svelte";
     import { ranking } from "../stores";
   
-    const a = 'haku';
+    const a = 'Ranking';
   
     //variavel declarada como array para receber o jason tratado
     let leitor = [];
@@ -12,7 +12,7 @@
         let texto = await resposta.text();
         let json = JSON.parse(texto);
         leitor = json;
-        console.log(leitor)
+        // console.log(leitor)
     }
   
     const form = {
@@ -21,46 +21,65 @@
     };
   
     const addranking = () => {
-          console.log(form.nome, form.pontos)
-          $ranking = $ranking.concat({
-              nome: form.nome,
-              pontos: form.pontos
-          })
-          console.log($ranking)
+        //   console.log(form.nome, form.pontos)
+        form.nome = 'Vasco';
+        form.pontos = 5699;
+
+        $ranking = $ranking.concat({
+            nome: form.nome,
+            pontos: form.pontos
+        })
+        //   console.log($ranking)
     }
   
     onMount(async () => {
       await loadRanking();
     });
-  
-    
   </script>
-  
-  
-  <h1>{a}</h1>
-  
-  <form on:submit|preventDefault={addranking}>
-      <input 
-          type="text" 
-          placeholder="Nome" 
-          name="Nome" 
-          bind:value={form.nome}
-      />
-      <input 
-          type="number" 
-          placeholder="pontos" 
-          name="pontos" 
-          bind:value={form.pontos}
-      /> 
-      <button>Mandar</button>
-  </form>
-  
-  {#each leitor as el, i }
-      {#if i < 10}
-          <ul>{(i+1) + "º"}  {el.Nome}  {el.pontos}</ul>
-      {/if}
-  {/each}
+  <body>
+    <div id="tela">
+        <h1>{a}</h1>
+        <!-- <form on:submit|preventDefault={addranking}>
+            <input 
+                type="text" 
+                placeholder="Nome" 
+                name="Nome" 
+                bind:value={form.nome}
+            />
+            <input 
+                type="number" 
+                placeholder="pontos" 
+                name="pontos" 
+                bind:value={form.pontos}
+            /> 
+        </form> -->
+        <button on:click={() => addranking()}>Mandar</button>
+        {#each leitor as el, i }
+            {#if i < 10}
+                <ul>{(i+1) + "º"}  {el.Nome}  {el.pontos}</ul>
+            {/if}
+        {/each}
+      </div>
+  </body>
   
   <style>
-  
+    body{
+        width: 100vw;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        display: flex;
+        background-color: gray;
+        margin: 0;
+        padding: 0;
+    }
+
+    #tela{
+        width: 1080px;
+        height: 720px;
+        justify-content: center;
+        align-items: center;
+        display: grid;  
+        border: 1px solid black
+    }
   </style>
